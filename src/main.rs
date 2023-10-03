6f84b384b7099f2609ad37ca86eadcf1ca07a325
Willow Hamid-Goodwin <willowgoodwin2006@gmail.com>
2023-10-03 17:25:07 +0100

    Extracted note generation method

--- src/main.rs
+++ src/main.rs
@@ -83,6 +83,71 @@ impl Note {
     }
 }
 
+fn generate_notes(mut note_array: [Note; 108]) -> [Note; 108] {
+    //note creation
+
+    //here are e templates used to generate a very large array of notes.
+    let note_name_pattern: [char; 12] = ['C', 'C', 'D','D', 'E', 'F', 'F', 'G','G', 'A', 'A', 'B'];
+    let accidental_pattern: [bool; 12] = [false, true, false, false, true, false, true, false, false, true, false, true];
+    let note_frequency_pattern: [f32; 12] = [16.35, 17.32, 18.35, 19.45, 20.60, 21.83, 23.12, 24.50, 25.96, 27.50, 29.14, 30.87];
+    let octave_multiplier_base: i32 = 2;
+
+    //initialisation of the array used to house the frequencies of notes, and then another for the actual completed note structs
+
+    //this changes how many notes are generated from C0
+
+    let mut note_frequencies: [f32; 108] = [0.0; 108];
+
+    let mut name_index = 0;
+    let mut accidental_index = 0;
+    let mut frequency_index = 0;
+    for n in 0..note_array.len() {
+        let mut oct = 0;
+
+        //putting names, accidentals, frequencies and octaves into a list of all notes
+
+        //this part loops the note letter array every 12 notes
+        if name_index > 11 {
+            name_index = name_index - 12
+        }
+
+        if accidental_index > 11 {
+            accidental_index = accidental_index - 12;
+        }
+
+        //this part increases the octave by 1 every 12 notes
+        if n >= 11 {
+            oct = oct + (1 * (n as i32 / 12))
+        }
+
+        //this part builds the frequencies array as we go
+        if frequency_index > 11 {
+            frequency_index = frequency_index - 12
+        }
+        if n <= 11 {
+            note_frequencies[n] = note_frequency_pattern[n];
+        }
+        else {
+            note_frequencies[n] = note_frequency_pattern[frequency_index] * octave_multiplier_base.pow(oct as u32) as f32;
+        }
+
+        //println!("{},{},{}", name_index, accidental_index, n);
+        //println!("{},{},{},{},", note_name_pattern[name_index], accidental_pattern[accidental_index], note_frequencies[n], oct);
+
+        note_array[n] = Note::new(note_name_pattern[name_index], accidental_pattern[accidental_index], note_frequencies[n], oct);
+
+        name_index = name_index + 1;
+        accidental_index = accidental_index + 1;
+        frequency_index = frequency_index + 1;
+    }
+
+    //for i in 0..note_array.len() {
+    //    println!("{},{},{},{}", note_array[i].name, note_array[i].accidental, note_array[i].frequency, note_array[i].octave);
+    //}
+
+    note_array
+}
+
 fn play_oscillator(oscillator1: WavetableOscillator, oscillator2: WavetableOscillator, oscillator3: WavetableOscillator, frequency1: f32, frequency2: f32, frequency3: f32) -> (WavetableOscillator, WavetableOscillator, WavetableOscillator){
     let mut temp_oscillator1 = WavetableOscillator::new(oscillator1.sample_rate.clone(), oscillator1.wave_table.clone());
     let mut temp_oscillator2 = WavetableOscillator::new(oscillator2.sample_rate.clone(), oscillator2.wave_table.clone());
@@ -134,74 +199,16 @@ fn main() {
         saw_wave_table.push(wavetable_function_saw);
     }
 
+    let mut note_array: [Note; 108] = [Note::new('a', false, 27.50, 0); 108];
+    note_array = generate_notes(note_array);
+    for i in 0..108 {
+        println!("{}", note_array[i].frequency);
+    }
     //creating oscillators with associated sample rates and wavetables
 
     let mut sine_oscillator = WavetableOscillator::new(44100, sine_wave_table);
     let mut square_oscillator = WavetableOscillator::new(44100, square_wave_table);
     let mut saw_oscillator = WavetableOscillator::new(44100, saw_wave_table);
-
-    //note creation
-
-    //here are e templates used to generate a very large array of notes.
-    let note_name_pattern: [char; 12] = ['C', 'C', 'D','D', 'E', 'F', 'F', 'G','G', 'A', 'A', 'B'];
-    let accidental_pattern: [bool; 12] = [false, true, false, false, true, false, true, false, false, true, false, true];
-    let note_frequency_pattern: [f32; 12] = [16.35, 17.32, 18.35, 19.45, 20.60, 21.83, 23.12, 24.50, 25.96, 27.50, 29.14, 30.87];
-    let octave_multiplier_base: i32 = 2;
-
-    //initialisation of the array used to house the frequencies of notes, and then another for the actual completed note structs
-
-    ///this changes how many notes are generated from C0
-    const NOTE_ARRAY_LEGNTH: usize = 108;
-
-    let mut note_frequencies: [f32; NOTE_ARRAY_LEGNTH] = [0.0; NOTE_ARRAY_LEGNTH];
-    let mut note_array: [Note; NOTE_ARRAY_LEGNTH] = [Note::new('a', false, 27.50, 0); NOTE_ARRAY_LEGNTH];
-
-    let mut name_index = 0;
-    let mut accidental_index = 0;
-    let mut frequency_index = 0;
-    for n in 0..note_array.len() {
-        let mut oct = 0;
-
-        //putting names, accidentals, frequencies and octaves into a list of all notes
-
-        //this part loops the note letter array every 12 notes
-        if name_index > 11 {
-            name_index = name_index - 12
-        }
-
-        if accidental_index > 11 {
-            accidental_index = accidental_index - 12;
-        }
-
-        //this part increases the octave by 1 every 12 notes
-        if n >= 11 {
-            oct = oct + (1 * (n as i32 / 12))
-        }
-
-        //this part builds the frequencies array as we go
-        if frequency_index > 11 {
-            frequency_index = frequency_index - 12
-        }
-        if n <= 11 {
-            note_frequencies[n] = note_frequency_pattern[n];
-        }
-        else {
-            note_frequencies[n] = note_frequency_pattern[frequency_index] * octave_multiplier_base.pow(oct as u32) as f32;
-        }
-
-        //println!("{},{},{}", name_index, accidental_index, n);
-        println!("{},{},{},{},", note_name_pattern[name_index], accidental_pattern[accidental_index], note_frequencies[n], oct);
-
-        note_array[n] = Note::new(note_name_pattern[name_index], accidental_pattern[accidental_index], note_frequencies[n], oct);
-
-        name_index = name_index + 1;
-        accidental_index = accidental_index + 1;
-        frequency_index = frequency_index + 1;
-    }
-
-    //for i in 0..note_array.len() {
-    //    println!("{},{},{},{}", note_array[i].name, note_array[i].accidental, note_array[i].frequency, note_array[i].octave);
-    //}
 
     (sine_oscillator,square_oscillator,saw_oscillator) = play_oscillator(sine_oscillator, square_oscillator, saw_oscillator, 262.0, 263.5, 260.5);
     (sine_oscillator,square_oscillator,saw_oscillator) = play_oscillator(sine_oscillator, square_oscillator, saw_oscillator, 393.0, 394.5, 391.5);
