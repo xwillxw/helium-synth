90026a01989bc1e3ad9014b08767196eff4af124
Willow Hamid-Goodwin <willowgoodwin2006@gmail.com>
2023-10-21 13:20:51 +0100

    Revert "Cleaned up play_oscillator()"
    This reverts commit 8d1b60b280fbfa0fd0dd4726a5bc32a3e128f9c8.

--- src/main.rs
+++ src/main.rs
@@ -1,35 +1,40 @@
 #![allow(unused_variables)]
-use startup::{Note, WavetableOscillator};
-use processing::filter_processor::Filter;
-
-use crate::startup::generate_patch;
+use startup::Note;
+use processing::filter_processor::{Filter,FilterType};
 mod startup;
 mod output;
 mod processing {
     pub mod filter_processor;
 }
 
-#[allow(dead_code)]
-pub struct SynthPatch {
-    oscillator_type: WavetableOscillator,
-    filter: Filter,
-}
-
 fn main() {
 
-    let patch = generate_patch();
+    //Generating array of notes, names and frequencies
     let note_array = startup::generate_notes();
+
+    //Generating oscillator types
+    let (sine,square,saw) = startup::generate_oscillators();
     let mut notes: Vec<Note> = Vec::new();
 
+    //Generating default filter
+    let current_filter: Filter = Filter::new(FilterType::LP,5000);
+
+
+    //playing music
     notes.push(note_array[50]);
     notes.push(note_array[54]);
     notes.push(note_array[57]);
-    output::play_oscillator(&patch, &notes, 1000);
+    output::play_oscillator(&square, &notes, 1000, &current_filter);
 
+    //changing filter type
+    println!("filter changed");
+    let current_filter: Filter = Filter::new(FilterType::HP,5000);
+
+    //playing music
     notes.clear();
     notes.push(note_array[52]);
     notes.push(note_array[54]);
     notes.push(note_array[57]);
-    output::play_oscillator(&patch, &notes, 1000);
+    output::play_oscillator(&square, &notes, 1000, &current_filter);
 
 }
